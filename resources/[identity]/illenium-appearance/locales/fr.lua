8f7ac347f1e9023a9128fdcbf94bf03c43ea5835
_mattin_b <mattin@berhau.fr>
2026-02-13 06:30:37 -0500

    Ajout oxmysql + dépendance + changement du spawn de base

---
+++ resources/[identity]/illenium-appearance/locales/fr.lua
@@ -0,0 +1,367 @@
+Locales["fr"] = {
+    UI = {
+        modal = {
+            save = {
+                title = "Enregistrer",
+                description = "tu resteras moche"
+            },
+            exit = {
+                title = "Quitter",
+                description = "Aucun changement ne sera enregistré"
+            },
+            accept = "Oui",
+            decline = "Non"
+        },
+        ped = {
+            title = "Ped",
+            model = "Modèle"
+        },
+        headBlend = {
+            title = "Héritage",
+            shape = {
+                title = "Visage",
+                firstOption = "Père",
+                secondOption = "Mère",
+                mix = "Mix"
+            },
+            skin = {
+                title = "Peau",
+                firstOption = "Père",
+                secondOption = "Mère",
+                mix = "Mix"
+            },
+            race = {
+                title = "Race",
+                shape = "Visage",
+                skin = "Peau",
+                mix = "Mix"
+            }
+        },
+        faceFeatures = {
+            title = "Caractéristiques du visage",
+            nose = {
+                title = "Nez",
+                width = "Largeur",
+                height = "Hauteur",
+                size = "Taille",
+                boneHeight = "Hauteur",
+                boneTwist = "Torsion",
+                peakHeight = "Hauteur maximale"
+            },
+            eyebrows = {
+                title = "Sourcils",
+                height = "Hauteur",
+                depth = "Profondeur"
+            },
+            cheeks = {
+                title = "Joues",
+                boneHeight = "Hauteur",
+                boneWidth = "Largeur 1",
+                width = "Largeur 2"
+            },
+            eyesAndMouth = {
+                title = "Yeux et bouche",
+                eyesOpening = "Ouverture des yeux",
+                lipsThickness = "Épaisseur des lèvres"
+            },
+            jaw = {
+                title = "Mâchoire",
+                width = "Largeur",
+                size = "Taille"
+            },
+            chin = {
+                title = "Menton",
+                lowering = "Abaissement",
+                length = "Longueur",
+                size = "Taille",
+                hole = "Taille du Creux"
+            },
+            neck = {
+                title = "Cou",
+                thickness = "Épaisseur"
+            }
+        },
+        headOverlays = {
+            title = "Apparence",
+            hair = {
+                title = "Cheveux",
+                style = "Style",
+                color = "Couleur",
+                highlight = "Highlight",
+                texture = "Texture",
+                fade = "Fade"
+            },
+            opacity = "Opacité",
+            style = "Style",
+            color = "Couleur",
+            secondColor = "Couleur de base",
+            blemishes = "Imperfections",
+            beard = "Barbe",
+            eyebrows = "Sourcils",
+            ageing = "Vieillissement",
+            makeUp = "Maquillage",
+            blush = "Blush",
+            complexion = "Complexion",
+            sunDamage = "Dégâts du soleil",
+            lipstick = "Lipstick",
+            moleAndFreckles = "Taches de rousseur",
+            chestHair = "Poils du torse",
+            bodyBlemishes = "Imperfections corporelles",
+            eyeColor = "Couleur des yeux"
+        },
+        components = {
+            title = "Vêtements",
+            drawable = "Drawable",
+            texture = "Texture",
+            mask = "Masque",
+            upperBody = "Gants",
+            lowerBody = "Bas du Corps",
+            bags = "Sacs et parachute",
+            shoes = "Chaussures",
+            scarfAndChains = "Écharpe et chaînes",
+            shirt = "T-Shirt",
+            bodyArmor = "Body armor",
+            decals = "Decals",
+            jackets = "Vestes",
+            head = "Tête"
+        },
+        props = {
+            title = "Props",
+            drawable = "Drawable",
+            texture = "Texture",
+            hats = "Chapeaux et casques",
+            glasses = "Lunettes",
+            ear = "Accessoires d'oreille",
+            watches = "Montres",
+            bracelets = "Bracelets"
+        },
+        tattoos = {
+            title = "Tattoos",
+            items = {
+                ZONE_TORSO = "Torso",
+                ZONE_HEAD = "Tête",
+                ZONE_LEFT_ARM = "Bras gauche",
+                ZONE_RIGHT_ARM = "Bras droit",
+                ZONE_LEFT_LEG = "Jambe gauche",
+                ZONE_RIGHT_LEG = "Jambe droite"
+            },
+            apply = "Appliquer",
+            delete = "Supprimer",
+            deleteAll = "Supprimer tous les tatouages",
+            opacity = "Opacité"
+        }
+    },
+    outfitManagement = {
+        title = "Gestion de tenues",
+        jobText = "Gérer les tenues pour le travail",
+        gangText = "Gérer les tenues pour le gang"
+    },
+    cancelled = {
+        title = "Personnalisation annulée",
+        description = "Personnalisation non enregistrée"
+    },
+    outfits = {
+        import = {
+            title = "Entrer le code de la tenue",
+            menuTitle = "Importer la tenue",
+            description = "Importer une tenue à partir d'un code de partage",
+            name = {
+                label = "Nom de la tenue",
+                placeholder = "Une belle tenue",
+                default = "Tenue importée"
+            },
+            code = {
+                label = "Code de la tenue"
+            },
+            success = {
+                title = "Tenue importée",
+                description = "Vous pouvez maintenant changer de tenue en utilisant le menu des tenues"
+            },
+            failure = {
+                title = "Échec de l'importation",
+                description = "Code de tenue non valide"
+            }
+        },
+        generate = {
+            title = "Générer un code de tenue",
+            description = "Générer un code de tenue pour le partage",
+            failure = {
+                title = "Quelque chose s'est mal passé",
+                description = "La génération du code a échoué pour la tenue"
+            },
+            success = {
+                title = "Code de tenue généré",
+                description = "Voici votre code de tenue"
+            }
+        },
+        save = {
+            menuTitle = "Enregistrer la tenue actuelle",
+            menuDescription = "Enregistrer votre tenue actuelle en tant que tenue %s",
+            description = "Enregistrer votre tenue actuelle",
+            title = "Nommez votre tenue",
+            managementTitle = "Détails de gestion des tenues",
+            name = {
+                label = "Nom de la tenue",
+                placeholder = "Tenue très cool"
+            },
+            gender = {
+                label = "Genre",
+                male = "Homme",
+                female = "Femme"
+            },
+            rank = {
+                label = "Rang minimum"
+            },
+            failure = {
+                title = "Échec de l'enregistrement",
+                description = "Une tenue avec ce nom existe déjà"
+            },
+            success = {
+                title = "Succès",
+                description = "Tenue %s enregistrée"
+            }
+        },
+        update = {
+            title = "Mettre à jour la tenue",
+            description = "Enregistrer vos vêtements actuels dans une tenue existante",
+            failure = {
+                title = "Échec de la mise à jour",
+                description = "Cette tenue n'existe pas"
+            },
+            success = {
+                title = "Succès",
+                description = "La tenue %s a été mise à jour"
+            }
+        },
+        change = {
+            title = "Changer de tenue",
+            description = "Choisissez parmi toutes vos tenues %s enregistrées",
+            pDescription = "Choisissez parmi toutes vos tenues enregistrées",
+            failure = {
+                title = "Quelque chose s'est mal passé",
+                description = "La tenue que vous essayez de changer n'a pas d'apparence de base",
+            }
+        },
+        delete = {
+            title = "Supprimer la tenue",
+            description = "Supprimer une tenue enregistrée %s",
+            mDescription = "Supprimer n'importe laquelle de vos tenues enregistrées",
+            item = {
+                title = 'Supprimer "%s"',
+                description = "Modèle : %s%s"
+            },
+            success = {
+                title = "Succès",
+                description = "Tenue supprimée"
+            }
+        },
+        manage = {
+            title = "👔 | Gérer les tenues %s"
+        }
+    },
+    jobOutfits = {
+        title = "Tenues de travail",
+        description = "Choisissez parmi toutes vos tenues de travail"
+    },
+    menu = {
+        returnTitle = "Retour",
+        title = "Chambre de vêtements",
+        outfitsTitle = "Tenues du joueur",
+        clothingShopTitle = "Magasin de vêtements",
+        barberShopTitle = "Salon de coiffure",
+        tattooShopTitle = "Salon de tatouage",
+        surgeonShopTitle = "Chirurgien esthétique"
+    },
+    clothing = {
+        title = "Acheter des vêtements - %d $",
+        titleNoPrice = "Changer de vêtements",
+        options = {
+            title = "👔 | Options de magasin de vêtements",
+            description = "Choisissez parmi une large gamme d'articles à porter"
+        },
+        outfits = {
+            title = "👔 | Options de tenue",
+            civilian = {
+                title = "Tenue civile",
+                description = "Mettre vos vêtements"
+            }
+        }
+    },
+    commands = {
+        reloadskin = {
+            title = "Recharger votre personnage",
+            failure = {
+                title = "Erreur",
+                description = "Vous ne pouvez pas utiliser reloadskin pour le moment"
+            }
+        },
+        clearstuckprops = {
+            title = "Supprimer tous les accessoires attachés à l'entité",
+            failure = {
+                title = "Erreur",
+                description = "Vous ne pouvez pas utiliser clearstuckprops pour le moment"
+            }
+        },
+        pedmenu = {
+            title = "Ouvrir / Donner le menu de vêtements",
+            failure = {
+                title = "Erreur",
+                description = "Joueur non connecté"
+            }
+        },
+        joboutfits = {
+            title = "Ouvre le menu des tenues de travail"
+        },
+        gangoutfits = {
+            title = "Ouvre le menu des tenues de gang"
+        },
+        bossmanagedoutfits = {
+            title = "Ouvre le menu des tenues gérées par le patron"
+        }
+    },
+    textUI = {
+        clothing = "Magasin de vêtements - Prix : %s $",
+        barber = "Barbier - Prix : %s $",
+        tattoo = "Boutique de tatouages - Prix : %s $",
+        surgeon = "Chirurgien plasticien - Prix : %s $",
+        clothingRoom = "Chambre d'habillage",
+        playerOutfitRoom = "Tenues"
+    },
+    migrate = {
+        success = {
+            title = "Succès",
+            description = "%s skins ont été migrées",
+            descriptionSingle = "Skin migré"
+        },
+        skip = {
+            title = "Information",
+            description = "Skin ignoré"
+        },
+        typeError = {
+            title = "Erreur",
+            description = "Type invalide"
+        }
+    },
+    purchase = {
+        tattoo = {
+            success = {
+                title = "Succès",
+                description = "Tatouage %s acheté pour %s$"
+            },
+            failure = {
+                title = "Échec de l'application du tatouage",
+                description = "Vous n'avez pas assez d'argent !"
+            }
+        },
+        store = {
+            success = {
+                title = "Succès",
+                description = "Don de %s$ à %s !"
+            },
+            failure = {
+                title = "Exploitation !",
+                description = "Vous n'avez pas assez d'argent ! Vous avez essayé d'exploiter le système !"
+            }
+        }
+    }
+}
